5fe5c8914197fa049f7d2a2821f873bc1c5e760e
budmc29 <chirica.mugurel@gmail.com>
2016-01-02 16:42:27 +0000

    add firefox tarball, check for program now working

--- firefox34.bash
+++ firefox34.bash
@@ -3,17 +3,16 @@
 # install firefox 34 for ubuntu
 
 # install wget if needed
-command -v wget >/dev/null 2>&1 || { apt-get install wget }
+command -v wget >/dev/null 2>&1 || { apt-get install wget; }
 
 # extract archive to folder
-wget ftp.mozilla.org/pub/mozilla.org/firefox/releases/34.0.5/linux-x86_64/en-US/firefox-34.0.5.tar.bz2
 tar -xjvf firefox-34.0.5.tar.bz2
 rm -rf /opt/firefox*
 mv firefox /opt/firefox34.0.5
 ln -sf /opt/firefox34.0.5/firefox /usr/bin/firefox
 
 # copy new icon to folder
-wget -o /opt/firefox34.0.5/34icon.png https://github.com/budmc29/firefox34/34icon.png
+wget -O /opt/firefox34.0.5/34icon.png https://github.com/budmc29/firefox34/34icon.png
 
 # create link for dash
 echo "[Desktop Entry]
