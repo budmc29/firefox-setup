97cb2d3d5a2c0e76f6a1c8ad88b8733a2fc8f038
budmc29 <chirica.mugurel@gmail.com>
2016-01-05 20:52:53 +0000

    add firefox plugins

--- firefox34.bash
+++ firefox34.bash
@@ -2,9 +2,6 @@
 
 # install firefox 34 for ubuntu
 
-# install wget if needed
-command -v wget >/dev/null 2>&1 || { apt-get install wget; }
-
 # extract archive to folder
 tar -xjvf firefox-34.0.5.tar.bz2
 rm -rf /opt/firefox*
@@ -12,7 +9,7 @@ mv firefox /opt/firefox34.0.5
 ln -sf /opt/firefox34.0.5/firefox /usr/bin/firefox
 
 # copy new icon to folder
-wget -O /opt/firefox34.0.5/34icon.png https://github.com/budmc29/firefox34/34icon.png
+cp 34icon.png /opt/firefox34.0.5/34icon.png
 
 # create link for dash
 echo "[Desktop Entry]
@@ -27,6 +24,11 @@ NoDisplay=false
 Categories=Internet;
 Name[en]=firefox.desktop" > /usr/share/applications/firefox.desktop
 
-# open firefox adblock plus extension
-xdg-open "adblock_plugin.xpi"&
 echo "Firefox installed"
+
+# open firefox adblock plus extension
+firefox "adblock.xpi"&
+firefox "dark_theme.xpi"&
+firefox "vimperator.xpi"&
+firefox "lastpass.xpi"&
+firefox "xmarks.xpi"&
--- readme.md
+++ readme.md
@@ -2,3 +2,9 @@
 ```
 sudo bash firefox34.bash
 ```
+Plugins:
+  Adblock
+  Ft deepdark 11beta28
+  Vimperator 3-9-fx
+  xmarks 4.3.7
+  Lastpass 3.2.42
